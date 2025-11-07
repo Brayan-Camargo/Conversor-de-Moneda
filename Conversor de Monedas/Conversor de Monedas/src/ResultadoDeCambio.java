9bbf5ffed6e2c460b2ba0b17782e417042c2162d
Brayan-Camargo <brayancamargoramirez@gmail.com>
2025-11-07 14:59:17 -0600

    se agrego la clase reultado de cambio y se agregaron getters y setter, ademas las estructura base del constructor para recibir datos de la API

--- Conversor de Monedas/Conversor de Monedas/src/ResultadoDeCambio.java
+++ Conversor de Monedas/Conversor de Monedas/src/ResultadoDeCambio.java
@@ -1,5 +1,56 @@
 public class ResultadoDeCambio {
-    private String CodigoMonedaOrigen;
-    private String CodigoMonedaResultado;
-    private double ValorDeTasaConversion;
+    private String monedaOrigen;
+    private String monedaResultado;
+    private double valorDeTasaConversion;
+
+    private double cantidadAConvertir;
+    private double resultaedoConversion;
+
+    //Aqui generaremos el constructor para inicializar con la API
+    public Transaccion(String origen, String destino, double tasa){
+        this.monedaOrigen = origen;
+        this.monedaResultado = destino;
+        this.valorDeTasaConversion = tasa;
+    }
+    
+
+    public String getMonedaOrigen() {
+        return monedaOrigen;
+    }
+
+    public void setMonedaOrigen(String monedaOrigen) {
+        this.monedaOrigen = monedaOrigen;
+    }
+
+    public String getMonedaResultado() {
+        return monedaResultado;
+    }
+
+    public void setMonedaResultado(String monedaResultado) {
+        this.monedaResultado = monedaResultado;
+    }
+
+    public double getValorDeTasaConversion() {
+        return valorDeTasaConversion;
+    }
+
+    public void setValorDeTasaConversion(double valorDeTasaConversion) {
+        this.valorDeTasaConversion = valorDeTasaConversion;
+    }
+
+    public double getCantidadAConvertir() {
+        return cantidadAConvertir;
+    }
+
+    public void setCantidadAConvertir(double cantidadAConvertir) {
+        this.cantidadAConvertir = cantidadAConvertir;
+    }
+
+    public double getResultaedoConversion() {
+        return resultaedoConversion;
+    }
+
+    public void setResultaedoConversion(double resultaedoConversion) {
+        this.resultaedoConversion = resultaedoConversion;
+    }
 }
